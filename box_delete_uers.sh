cdebb7c954fcfe1c25c1482aa30f8340942e7390
Ernesto Espinosa <eespinosa@nostromo.lsit.ucsb.edu>
2018-07-09 08:48:10 -0700

    First commit

---
+++ box_delete_uers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#LIST=`for i in $(cat test.txt ) ; do box users search $i | grep \"id\" | awk -F '"' '{print $4}'; done`
+
+
+#for i in $(cat $LIST) ; do box users delete -f -y $i ; done
+
+#Create list of user accounts from text file
+declare -a userList=`cat test.txt`
+
+declare -a idList=()
+
+for i in ${userList[@]}; do
+	idSearch=$(do box users search $i | grep \"id\" | awk -F '"' '{print $4}')
+	idList+=( $idSearch )
+done
+
+
+for i in ${idList[@]}; do
